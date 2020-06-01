c89059d237f0e326ecd424b97265422c4cfc7601
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-06-01 14:01:09 +0300

    First commit. Prepare working enviroment

---
+++ webpack.config.js
@@ -0,0 +1,133 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+    const config = {};
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCSSAssetsPlugin({
+                cssProcessor: require('cssnano'),
+                cssProcessorPluginOptions: {
+                    preset: ['default', {
+                        discardComments: {
+                            removeAll: true,
+                        },
+                    }],
+                },
+            }),
+            new TerserPlugin(),
+        ];
+    }
+
+    return config;
+};
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'project.min.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 4100,
+    },
+    optimization: optimization(),
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css',
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [autoprefixer()],
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+            minify: false,
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {},
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[folder]/[name].[ext]',
+                        },
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 85,
+                            }, // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [
+                                    0.8,
+                                    0.9,
+                                ],
+                                speed: 7,
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            }, // the webp option will enable WEBP
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot|)$/i,
+                loader: 'file-loader',
+            },
+        ],
+    },
+};
