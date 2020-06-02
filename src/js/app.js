80fd7957ded03048e6cbdcee4a84e289ba02d12f
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-06-02 14:46:54 +0300

    Добавил второй параметр в виде объекта с функциями. Ключ объекта - это тип, указанный в data-type у самого input, а значение - это функция валидации

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,72 @@
 import is from 'is_js';
 
+
+/**
+ * validateFunctions = {email: fn()}
+ */
+
 class ValidationInput {
-  constructor(selector) {
+  constructor(selector, validateFunctions = {}) {
     this.$el = document.querySelector(selector);
+    this.$inputs = this.$el.querySelectorAll('[data-input]');
+    this.$validateFunctions = validateFunctions;
     this.submit = this.submit.bind(this); // Привязка к контексту
-    this.checkAllValidate = this.checkAllValidate.bind(this); // Привязка к контексту
+    this.input = this.input.bind(this); // Привязка к контексту
+    this.flag = true;
   }
 
   init() {
     this.$el.addEventListener('submit', this.submit);
+
+    for (let i = 0; i < this.$inputs.length; i++) {
+      this.$inputs[i].addEventListener('input', this.input);
+    }
+  }
+
+  /**
+   * По ключу type Я буду получать функцию из объекта validateFunctions
+   * @param {event} e
+   */
+  input(e) {
+    const { type } = e.target.dataset;
+    const fn = this.$validateFunctions[type];
+
+    // Проверка на случай отсутствия переданного типа и функции
+    if (fn) {
+      this.flag = fn(e);
+    }
   }
 
   submit(e) {
     e.preventDefault();
-
-    const flag = this.checkAllValidate();
-
-    if (flag) {
-      this.$el.submit();
-    }
   }
+}
 
-  checkAllValidate() {
-    let flag = true;
+const params = {
+  email(e) {
+    const email = e.target;
+    const parent = email.parentElement;
+    const small = parent.querySelector('small');
 
-    if (!this.validateEmail('Введите корректный email')) {
-      flag = false;
-    }
+    const result = is.email(email.value);
 
-    if (!this.validatePassword('Пароль должен быть более 6 символов')) {
-      flag = false;
-    }
 
-    if (flag) {
-      // eslint-disable-next-line no-console
-      console.log('VALID');
-      // Показываем всплывашку, очищаем форму
+    if (!result) {
+      parent.classList.add('invalid');
+      small.classList.add('visible');
+
+      // if (errorMessage) {
+      //   small.innerHTML = errorMessage;
+      // }
     } else {
-      // eslint-disable-next-line no-console
-      console.log('INVALID');
+      parent.classList.remove('invalid');
+      small.classList.remove('visible');
     }
 
-    return flag;
-  }
-
-  validatePassword(errorMessage) {
-    const password = document.querySelector('[data-input="password"]');
+    return result;
+  },
+  password(e) {
+    const password = e.target;
     const parent = password.parentElement;
     const small = parent.querySelector('small');
 
@@ -59,42 +80,40 @@ class ValidationInput {
     if (!flag) {
       parent.classList.add('invalid');
       small.classList.add('visible');
-
-      if (errorMessage) {
-        small.innerHTML = errorMessage;
-      }
+      //
+      // if (errorMessage) {
+      //   small.innerHTML = errorMessage;
+      // }
     } else {
       parent.classList.remove('invalid');
       small.classList.remove('visible');
     }
 
     return flag;
-  }
-
-  validateEmail(errorMessage) {
-    const email = document.querySelector('[data-input="email"]');
-    const parent = email.parentElement;
+  },
+  text(e) {
+    const text = e.target;
+    const parent = text.parentElement;
     const small = parent.querySelector('small');
 
-    const result = is.email(email.value);
+    let flag = true;
+    // логика проверки
 
+    if (text.value.length <= 5) {
+      flag = false;
+    }
 
-    if (!result) {
+    if (!flag) {
       parent.classList.add('invalid');
       small.classList.add('visible');
-
-      if (errorMessage) {
-        small.innerHTML = errorMessage;
-      }
     } else {
       parent.classList.remove('invalid');
       small.classList.remove('visible');
     }
 
-    return result;
-  }
-}
+    return flag;
+  },
+};
 
-const form = new ValidationInput('#signInForm');
-
+const form = new ValidationInput('#signInForm', params);
 form.init();
